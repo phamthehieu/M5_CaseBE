b7bd5524440c9a2232c3b80e3d418374d08840f1
phamthehieu <111071418+phamthehieu@users.noreply.github.com>
2023-03-27 22:36:30 +0700

    1

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,8 @@ import cors from "cors"
 import {router} from "./router/router";
 const http = require("http");
 const app = express();
-import { Request, Response } from 'express'
 AppDataSource.initialize().then(() => {
     console.log(" Connect Server! ")
-})
-app.get('/', (_req: Request, res: Response) => {
-    return res.send('Express Typescript on Vercel')
-})
-
-app.get('/ping', (_req: Request, res: Response) => {
-    return res.send('pong 🏓')
 })
 app.use(cors());
 app.use(bodyParser.json());
